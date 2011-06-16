565db505784067927172147e9dfd38b43dd34990
Ben Kamens <ben@khanacademy.org>
2011-06-16 15:07:35 +0700

    Accept cl-arguments for different steps of export process so we can run one at a time

--- export.py
+++ export.py
@@ -1,3 +1,4 @@
+import optparse
 import os
 
 import api
@@ -8,22 +9,16 @@ import zencode
 class YouTubeExporter(object):
 
     MAX_CONVERT_PER_RUN = 1
-    MAX_UPLOAD_PER_RUN = 1
-
-    @staticmethod
-    def start():
-        YouTubeExporter.handle_new_videos()
-        #YouTubeExporter.handle_converted_videos()
+    MAX_PUBLISH_PER_RUN = 1
 
     # Start export for all videos that haven't been converted to downloadable format
     @staticmethod
-    def handle_new_videos():
-        print "Searching for unconverted YouTube videos"
+    def convert_new_videos():
+        print "Searching for unconverted videos"
 
         videos = api.list_new_videos()[:YouTubeExporter.MAX_CONVERT_PER_RUN]
         for video in videos:
-
-            print "Starting with youtube id %s" % video["youtube_id"]
+            print "Starting conversion with youtube id %s" % video["youtube_id"]
 
             youtube_id, video_filename, video_path = youtube.download(video)
             print "Downloaded video to %s" % video_path
@@ -40,13 +35,36 @@ class YouTubeExporter(object):
     # Finish export for all videos that have been converted to downloadable format
     # but haven't been uploaded to public host yet
     @staticmethod
-    def handle_converted_videos():
-        videos_s3 = s3.list_converted_videos()[:YouTubeExporter.MAX_UPLOAD_PER_RUN]
-        for video_s3 in videos_s3:
-            archive.upload_from_s3(video_s3)
+    def publish_converted_videos():
+        print "Searching for converted videos"
+
+        s3_urls = s3.list_converted_video_urls()[:YouTubeExporter.MAX_PUBLISH_PER_RUN]
+        for s3_url in s3_urls:
+            print "Starting publish with %s" % s3_url
+
+            archive_url = archive.upload_from_s3(s3_url)
+            print "Uploaded via archive.org to %s" % archive_url
+
+            # TODO: check HEAD request on archive.org
+            # TODO: delete both versions from s3
+            # TODO: update KA API record
 
 def main():
-    YouTubeExporter.start()
-    
+
+    parser = optparse.OptionParser()
+
+    parser.add_option('-s', '--step',
+        action="store", dest="step",
+        help="Export step ('convert' or 'publish' currently)", default="convert")
+
+    options, args = parser.parse_args()
+
+    if options.step == "convert":
+        YouTubeExporter.convert_new_videos()
+    elif options.step == "publish":
+        YouTubeExporter.publish_converted_videos()
+    else:
+        print "Unknown export step."
+
 if __name__ == "__main__":
     main()
