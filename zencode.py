7d9836a1352c29332467f22a60fbd390b7a046d4
Adam Ernst <adamernst@cosmicsoft.net>
2011-06-17 22:19:22 +0400

    Set base_url directly on thumbnails as otherwise it ends up in Zencoder's own bucket. Also match filename to the hierarchical structure we use for videos.

--- zencode.py
+++ zencode.py
@@ -1,11 +1,13 @@
 from secrets import zencoder_api_key
 from zencoder import Zencoder
 
+BASE_URL = "https://s3.amazonaws.com/KA-youtube-converted/"
+
 def start_converting(youtube_id, s3_url, thumbnail_time):
     zen = Zencoder(zencoder_api_key)
 
     output_config = {
-        "base_url": "https://s3.amazonaws.com/KA-youtube-converted/",
+        "base_url": BASE_URL,
         "filename": "%s/%s.mp4" % (youtube_id, youtube_id),
         "video_codec": "h264",
         "quality": 5,
@@ -15,9 +17,10 @@ def start_converting(youtube_id, s3_url, thumbnail_time):
 
     if thumbnail_time is not None:
         output_config["thumbnails"] = {
+            "base_url": BASE_URL,
             "times": [thumbnail_time], 
             "public": 1,
-            "filename": "%s" % (youtube_id,),
+            "filename": "%s/%s" % (youtube_id, youtube_id),
         }
 
     job = zen.job.create(s3_url, outputs=output_config)
