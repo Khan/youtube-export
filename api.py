4d50b35e02455f9aa9cb81786fd78e136e4b20fe
Ben Alpert <alpert@khanacademy.org>
2012-09-02 05:48:05 +0400

    Change youtube-export to use the new secret key instead of OAuth
    Summary: (Also see D706.)
    
    Reviewers: kamens
    
    Differential Revision: http://phabricator.khanacademy.org/D708

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
-import logging
+import urllib
 import urllib2
 import simplejson
 
-from oauth import OAuthConsumer, OAuthToken, OAuthRequest, OAuthSignatureMethod_HMAC_SHA1
-from secrets import ka_consumer_key, ka_consumer_secret, ka_access_token, ka_access_token_secret
+import secrets
 from util import logger, DOWNLOADABLE_FORMATS
 
 _library = None
+
+
 def get_library():
     global _library
     if _library is None:
@@ -17,6 +18,7 @@ def get_library():
             fresh.close()
     return _library
 
+
 def list_videos():
     for playlist in get_library():
         for video in playlist["videos"]:
@@ -24,8 +26,9 @@ def list_videos():
                 continue
             yield video
 
+
 def list_missing_video_content():
-    """Returns a dictionary mapping youtube IDs to formats missing from the API"""
+    """Return a dict mapping youtube IDs to formats missing from the API."""
 
     missing_content = {}
 
@@ -39,29 +42,24 @@ def list_missing_video_content():
 
     return missing_content
 
+
 def video_metadata(youtube_id):
-    """Returns metadata dict (title, description, etc.) for a given youtube_id."""
+    """Returns metadata dict (title, description, etc.) for a given
+    youtube_id."""
     for video in list_videos():
         if video["youtube_id"] == youtube_id:
             return video
 
+
 def update_download_available(youtube_id, available_formats):
 
-    consumer = OAuthConsumer(ka_consumer_key, ka_consumer_secret)
-    access_token = OAuthToken(ka_access_token, ka_access_token_secret)
     url = "http://www.khanacademy.org/api/v1/videos/%s/download_available" % youtube_id
+    params = {
+        'formats': ','.join(available_formats),
+        'key': secrets.ka_download_available_secret,
+    }
 
-    oauth_request = OAuthRequest.from_consumer_and_token(
-            consumer,
-            token = access_token,
-            http_url = url,
-            http_method="POST",
-            parameters={"formats": ",".join(available_formats)},
-            )
-
-    oauth_request.sign_request(OAuthSignatureMethod_HMAC_SHA1(), consumer, access_token)
-
-    response = urllib2.urlopen(oauth_request.get_normalized_http_url(), data=oauth_request.to_postdata())
+    response = urllib2.urlopen(url, data=urllib.urlencode(params))
     logger.info(response.read())
 
     return response.code == 200
