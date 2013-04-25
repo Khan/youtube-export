4f33ba33141fc7d52b2893a04876341956c79de1
Ben Alpert <spicyjalapeno@gmail.com>
2013-04-24 22:59:55 -0700

    Various nits and fixes
    - languagechannels/utils.py now passes lint (80-char lines, mostly)
    - use argparse instead of deprecated optparse
    - changed exception handling to just bail out if the YouTube API is
    unavailable for some reason and log an error instead of saving invalid
    or incomplete data

--- languagechannels/utils.py
+++ languagechannels/utils.py
@@ -1,12 +1,18 @@
-import optparse
-import logging
-import urllib2
+import argparse
+import errno
 import json
-import pdb
-import csv
-import time
-from known_channels import known_language_channels
+import logging
 import os
+import sys
+import time
+import urllib2
+
+from known_channels import known_language_channels
+
+
+def _channel_json_filename(channel_id):
+    return os.path.join(os.path.dirname(__file__), 'youtube_data',
+            '%s.json' % channel_id)
 
 
 def update_all_language_channels_json():
@@ -22,6 +28,7 @@ def update_language_channel_json(channel_id):
     """Write a new JSON file for specified channel."""
     uploads_json = channel_uploads(channel_id)
     playlist_ids_list = playlist_ids(channel_id)
+
     playlist_videos_json = {}
     for pl_id in playlist_ids_list:
         playlist_videos_json[pl_id] = playlist_videos(pl_id)
@@ -29,14 +36,10 @@ def update_language_channel_json(channel_id):
         "video_uploads": uploads_json,
         "playlist_videos": playlist_videos_json
     }
-    directory = os.path.dirname(os.path.abspath(__file__)) + '/youtube_data/'
-    filename = '%s.json' % channel_id
-    full_file_path = directory + filename
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        logging.info("Created directory %s." % directory)
-    logging.info("Writing data to %s." % full_file_path)
-    with open(full_file_path, 'wb') as fp:
+
+    filename = _channel_json_filename(channel_id)
+    logging.info("Writing data to %s." % filename)
+    with open(filename, 'wb') as fp:
         json.dump(full_channel_json, fp)
 
 
@@ -50,38 +53,23 @@ def channel_uploads(channel_id):
     more_videos = True
     while more_videos:
         url = "https://gdata.youtube.com/feeds/api/users/%s/uploads?alt=json&max-results=50&start-index=%d" % (channel_id, start_index)
-        try:
-            response = make_request(url)
-        except Exception, e:
-            logging.info("Setting more_videos to false to continue execution.")
+        response = make_request(url)
+        entry = response["feed"].get("entry")
+        if entry is None:
             more_videos = False
         else:
-            entry = response.get("feed").get("entry")
-            if entry == None:
-                more_videos = False
-            else:
-                start_index += 50
-                video_entries += entry
+            start_index += 50
+            video_entries += entry
     return video_entries
 
 
 def playlist_ids(channel_id):
     """Return the combined JSON response of all the channel's playlists."""
     logging.info("Getting all playlists for %s" % channel_id)
-    playlist_ids = []
     url = "https://gdata.youtube.com/feeds/api/users/%s/playlists?v=2&alt=json&max-results=50" % channel_id
-    try:
-        response = make_request(url)
-    except Exception, e:
-        return playlist_ids
-    else:
-        entry = response.get("feed").get("entry")
-        if entry == None:
-            return playlist_ids
-        else:
-            for playlist_id in entry:
-                playlist_ids.append(playlist_id["yt$playlistId"]["$t"])
-            return playlist_ids
+    response = make_request(url)
+    playlists = response["feed"].get("entry", [])
+    return [playlist["yt$playlistId"]["$t"] for playlist in playlists]
 
 
 def playlist_videos(playlist_id):
@@ -91,25 +79,19 @@ def playlist_videos(playlist_id):
     start_index = 1
     more_videos = True
     while more_videos:
-        url = "http://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=json&max-results=50&start-index=%d" % (
-            playlist_id, start_index)
-        try:
-            response = make_request(url)
-        except Exception, e:
-            logging.info("Setting more_videos to false to continue execution.")
+        url = "http://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=json&max-results=50&start-index=%d" % (playlist_id, start_index)
+        response = make_request(url)
+        entry = response["feed"].get("entry")
+        if entry is None:
             more_videos = False
         else:
-            entry = response.get("feed").get("entry")
-            if entry == None:
-                more_videos = False
-            else:
-                start_index += 50
-                playlist_entries += entry
+            start_index += 50
+            playlist_entries += entry
     return playlist_entries
 
 
 def make_request(url):
-    """Make an API request and handle errors, retry up to 5 times."""
+    """Make an API request and handle errors, retrying up to 5 times."""
     for n in range(5):
         time.sleep(1)
         try:
@@ -117,10 +99,10 @@ def make_request(url):
             response = json.load(urllib2.urlopen(request))
         except Exception, e:
             logging.error(
-                "Error during request. Trying again %d/5 times." % (n+1))
-            logging.debug("Error: %s.\nURL: %s" % (e, url))
+                "Error during request. Trying again %d/5 times.", n + 1)
+            logging.debug("Error: %s.\nURL: %s", e, url)
             if n == 5:
-                raise Exception("Error during request.")
+                raise
         else:
             return response
 
@@ -130,21 +112,17 @@ def video_ids_set(channel_ids=None):
     Return all video IDs if left empty.
     """
     if channel_ids:
-        logging.info("Returning set of video IDs for %s" %
-                     ", ".join(channel_ids))
+        logging.info(
+                "Returning set of video IDs for %s", ", ".join(channel_ids))
     else:
         logging.info("Returning set of all language channel video IDs")
         channel_ids = known_language_channels.keys()
 
     video_ids = set()
     for channel_id in channel_ids:
-        try:
-            ensure_existence(channel_id)
-        except Exception, e:
-            logging.error(
-                "'%s' is not a language channel. Check for misspellings and try again! :)" % channel_id)
-        else:
-            video_ids.update(extract_ids(channel_id))
+        if channel_id not in known_language_channels:
+            raise KeyError("Unknown language channel %r" % channel_id)
+        video_ids.update(extract_ids(channel_id))
     return video_ids
 
 
@@ -153,74 +131,90 @@ def extract_ids(channel_id):
     playlists of the channel.
     """
     video_ids = set()
-    data = json.load(open(os.path.dirname(os.path.realpath(
-        __file__)) + '/youtube_data/%s.json' % channel_id))
+    filename = _channel_json_filename(channel_id)
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+    except IOError, e:
+        if e.errno == errno.ENOENT:
+            logging.warning("File %s not found, skipping", filename)
+            return set()
+        else:
+            raise
+
     # Extract uploaded video IDs
     for entry in data["video_uploads"]:
         video_id = entry["id"]["$t"].replace(
             "http://gdata.youtube.com/feeds/api/videos/", "")
         assert len(video_id) == 11
         video_ids.add(video_id)
+
     # Extract playlist video IDs
     for playlist_id in data["playlist_videos"]:
         for video in data["playlist_videos"][playlist_id]:
             video_ids.add(video["media$group"]["yt$videoid"]["$t"])
+
     return video_ids
-
-
-def ensure_existence(channel_id):
-    """Ensure that the language channel ID given exists inside the language
-    channel dictionary.
-    """
-    if channel_id not in known_language_channels:
-        raise Exception(
-            "'%s' is not a language channel. Check for misspellings and try again! :)" % channel_id)
 
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+            format='%(levelname)s\t%(asctime)s\t%(message)s')
 
 
 def main():
-    parser = optparse.OptionParser()
+    parser = argparse.ArgumentParser()
 
-    parser.add_option("-U", "--update", action="store_true", dest="update",
-                      help="Request updated data on language channels via the YouTube API.",
-                      default=False)
+    parser.add_argument(
+            "-U", "--update", action="store_true", dest="update",
+            help="Request updated data on language channels via the YouTube "
+            "API.")
 
-    parser.add_option("-l", "--language-channel", action="append",
-                      dest="language_channels", help="Languages to update. Ex: -l "
-                      "'KhanAcademyRussian' -l 'KhanAcademyDansk'. Default is all.",
-                      default=None)
+    parser.add_argument(
+            "-l", "--language-channel", action="append",
+            dest="language_channels", default=None,
+            help="Languages to update. Ex: -l 'KhanAcademyRussian' "
+            "-l 'KhanAcademyDansk'. Default is all.")
 
-    parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
-                      help="Suppress output.")
+    parser.add_argument(
+            "-q", "--quiet", action="store_true",
+            help="Suppress log output.")
 
-    parser.add_option("-v", "--videos", action="store_true", dest="videos_set",
-                      help="Return a set of videos for the specified language channels. "
-                      "If language channels are not specified it will return a set of all "
-                      "video ids for all language channels.")
+    parser.add_argument(
+            "-v", "--videos", action="store_true", dest="videos_set",
+            help="Print a list of video IDs for the specified "
+            "language channels. If language channels are not "
+            "specified it will return a set of all video ids for "
+            "all language channels.")
 
-    options, args = parser.parse_args()
+    args = parser.parse_args()
 
-    if not options.quiet:
+    if not args.videos_set and not args.update:
+        parser.print_help()
+        return 1
+
+    if not args.quiet:
         setup_logging()
 
-    if options.update:
-        if not options.language_channels:
-            update_all_language_channels_json()
-        else:
-            for language_ch in options.language_channels:
-                if ensure_existence(language_ch):
-                    update_language_channel_json(language_ch)
+    if args.update:
+        channels = args.language_channels or known_language_channels
+        for channel_id in channels:
+            if channel_id not in known_language_channels:
+                logging.error("Unknown language channel %r", channel_id)
+                return 1
 
-    if options.videos_set:
-        return video_ids_set(options.language_channels)
+            try:
+                update_language_channel_json(channel_id)
+            except Exception:
+                logging.exception(
+                        "Unable to update JSON for channel %r", channel_id)
 
-    if not options.videos_set and not options.language_channels and not options.update:
-        logging.info("Use --help to see runnable options.")
+    if args.videos_set:
+        for video_id in video_ids_set(args.language_channels):
+            print video_id
+
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
