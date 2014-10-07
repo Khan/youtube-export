7a5e237e93fa759f94f82a22e15222234a6f835b
Craig Silverstein <csilvers@khanacademy.org>
2014-10-07 16:54:34 -0700

    Another file we need to adjust secrets in.
    Auditors: alpert
    
    Test Plan:
    Fingers crossed.

--- zencode.py
+++ zencode.py
@@ -1,9 +1,10 @@
 import logging
 import youtube
 
-from secrets import zencoder_api_key
 from zencoder import Zencoder
 from util import logger
+
+zencoder_api_key = open(expanduser('~/zencoder_api_key')).read().strip()
 
 BASE_URL = "https://s3.amazonaws.com/KA-youtube-converted/"
 
