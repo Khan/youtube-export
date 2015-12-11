afebffce71a332ed278d408af43fbf4f3502866e
David Hu <davidhu91@gmail.com>
2015-12-10 17:29:25 -0800

    Add script to aggressively compress an entire topic
    Summary:
    This script fetches all YouTube video IDs under a given topic, and then uses Zencoder to encode with aggressive encoding parameters. We'd like to use this to test and tweak compression settings.
    
    Compressed output currently goes to a test bucket, `https://s3.amazonaws.com/ka-david-test-bucket/`.
    
    This was already uploaded and run for the `addition-subtraction` topic. Here's a spreadsheet with links to the output:
    
    https://docs.google.com/spreadsheets/d/1VvhZcHiDM_VDGUSESc5vuAC7f7mJoUd6zB75xXUMxns/edit#gid=0
    
    For project [Video compression infra: API and youtube-export](https://docs.google.com/document/d/1eI2NQIQ88uZwW-qArupC2QBpwMSOm4aqwyTBf361MAE/edit#)
    Asana: https://app.asana.com/0/72610290451048/72610290451049
    
    Test Plan:
    Upload and run this on the `ka-youtube-export` machine:
    
    ```
    cd ..
    scp -r youtube-export ka-youtube-export:~/youtube-export-test
    ssh ka-youtube-export
    cd youtube-export-test
    python aggressively_compress_topic.py addition-subtraction
    ```
    
    Go to https://app.zencoder.com/jobs. See that jobs have been successfully submitted and run.
    
    Go to https://console.aws.amazon.com/s3/home?region=us-east-1#&bucket=ka-david-test-bucket. See outputted videos. Example:
    
    normal: https://s3.amazonaws.com/ka-david-test-bucket/OJ-wajo6oa4.mp4/OJ-wajo6oa4.mp4
    aggressively-compressed: https://s3.amazonaws.com/ka-david-test-bucket/OJ-wajo6oa4.mp4/OJ-wajo6oa4-low.mp4
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    Subscribers: andy, matt
    
    Differential Revision: https://phabricator.khanacademy.org/D24014

---
+++ aggressively_compress_topic.py
@@ -0,0 +1,76 @@
+"""Encode all videos under a single topic with aggressive compression settings.
+
+This is for testing out and tweaking compression settings.
+"""
+import argparse
+import contextlib
+import json
+import urllib2
+
+import zencode
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+            description="Aggressively compress videos under a given topic")
+
+    parser.add_argument('topic_slug',
+            help="Slug of topic to encode videos for")
+
+    parser.add_argument("--base-url",
+            default="https://s3.amazonaws.com/ka-david-test-bucket/",
+            help="Base S3 URL for output")
+
+    parser.add_argument("--dry-run", action="store_true", default=False,
+            help="Don't start Zencoder jobs; just print videos to be encoded")
+
+    return parser.parse_args()
+
+
+def get_youtube_ids(topic_slug):
+    """Get a list of all youtube IDs for a single topic."""
+    # We get the entire topictree instead of using the /topic/<topic>/videos
+    # endpoint because that one doesn't seem to work for non-terminal topics
+    # (e.g. subjects).
+    api_url = "https://www.khanacademy.org/api/v1/topictree?kind=Video"
+    with contextlib.closing(urllib2.urlopen(api_url)) as topictree_request:
+        topictree_root = json.load(topictree_request)
+
+    youtube_ids = []
+
+    def _traverse_tree(node, seen_our_topic_slug):
+        if node["kind"] == "Video" and seen_our_topic_slug:
+            youtube_ids.append(node["youtube_id"])
+        elif node["kind"] == "Topic":
+            seen_our_topic_slug |= (node["slug"] == topic_slug)
+            for child in node["children"]:
+                _traverse_tree(child, seen_our_topic_slug)
+
+    _traverse_tree(topictree_root, False)
+    return youtube_ids
+
+
+def main():
+    args = get_arguments()
+
+    print "Fetching video YouTube IDs under topic %s" % args.topic_slug
+    youtube_ids = get_youtube_ids(args.topic_slug)
+
+    for youtube_id in youtube_ids:
+        source_url = "s3://KA-youtube-converted/%s.mp4/%s.mp4" % (
+                youtube_id, youtube_id)
+        print "Converting YouTube video %s on Zencoder (source url: %s)" % (
+                youtube_id, source_url)
+        if not args.dry_run:
+            formats_to_create = ["mp4_aggressive_compression_test", "mp4",
+                    "m3u8", "m3u8_aggressive_compression_test"]
+            zencode.start_converting(youtube_id, source_url,
+                    formats_to_create, base_url=args.base_url)
+
+    print
+    print "See %s running jobs at https://app.zencoder.com/jobs" % len(
+            youtube_ids)
+
+
+if __name__ == '__main__':
+    main()
--- zencode.py
+++ zencode.py
@@ -12,15 +12,27 @@ BASE_URL = "https://s3.amazonaws.com/KA-youtube-converted/"
 def output_types():
     return {
         "mp4": [output_mp4],
+
+        # NOTE: Experimental aggressive compression settings -- not to be part
+        #     of production processing yet.
+        "mp4_aggressive_compression_test": [output_mp4_low],
+
         "m3u8": [
             output_m3u8_playlist,
             output_m3u8_low,
             output_m3u8_medium,
             output_m3u8_high,
-        ]
+        ],
+
+        # NOTE: Experimental aggressive compression settings -- not to be part
+        #     of production processing yet.
+        "m3u8_aggressive_compression_test": [
+            output_m3u8_playlist_aggressively_compressed,
+            output_m3u8_aggressively_compressed,
+        ],
     }
 
-def start_converting(youtube_id, s3_url, formats_to_create):
+def start_converting(youtube_id, s3_url, formats_to_create, base_url=BASE_URL):
 
     # TODO(csilvers): figure out how to get thumbnail times from youtube APIv3
     #thumbnail_time = youtube.get_thumbnail_time(youtube_id)
@@ -31,7 +43,8 @@ def start_converting(youtube_id, s3_url, formats_to_create):
 
     for format_to_create in formats_to_create:
         if format_to_create in output_types():
-            outputs += [fxn(youtube_id, thumbnail_time) for fxn in output_types()[format_to_create]]
+            outputs += [fxn(youtube_id, thumbnail_time, base_url)
+                    for fxn in output_types()[format_to_create]]
     
     job_response = zen.job.create(s3_url, outputs=outputs)
 
@@ -39,15 +52,68 @@ def start_converting(youtube_id, s3_url, formats_to_create):
 
     logger.info("Zencoder job created successfully")
 
-def output_mp4(youtube_id, thumbnail_time):
+def output_mp4_low(youtube_id, thumbnail_time, base_url):
     output = {
-        "base_url": BASE_URL,
-        "filename": "%s.mp4/%s.mp4" % (youtube_id, youtube_id),
-        "video_codec": "h264",
-        "tuning": "animation",
-        "quality": 5,
-        "speed": 1,
+        "base_url": base_url,
+        "filename": "%s.mp4/%s-low.mp4" % (youtube_id, youtube_id),
         "public": 1,
+        "speed": 1,
+        "tuning": "animation",
+
+        # We're using the 3GP container format which supports audio streams
+        # encoded with the AMR codec.
+        "format": "3gp",
+
+        # Video encoding options
+        "video_codec": "h264",
+        "crf": 40,
+        "max_frame_rate": 15,
+        "size": "640x480",
+
+        # Audio encoding options
+        "audio_codec": "amr",
+        "audio_bitrate": 4,
+        "audio_sample_rate": 8000,
+
+        "watermarks": [
+            {
+                "width": 128,
+                "height": 16,
+                "x": -2,
+                "y": -2,
+                "url": "http://www.khanacademy.org/images/watermark.png",
+            }
+        ]
+    }
+
+    # TODO(david): The output folder+filename is the same in this output
+    #    function as output_mp4 below. This could mean that if we process with
+    #    output_mp4 then output_mp4_low, we'll end up with the low-quality
+    #    thumbnail for the regular mp4 video too. However, thumbnail extraction
+    #    is not currently working. If we do want to make it work, look into
+    #    this.
+    if thumbnail_time is not None:
+        output["thumbnails"] = {
+            "times": [thumbnail_time],
+            "public": 1,
+            "base_url": "{0}{1}.mp4/".format(base_url, youtube_id),
+            "filename": youtube_id,
+        }
+
+    return output
+
+def output_mp4(youtube_id, thumbnail_time, base_url):
+    output = {
+        "base_url": base_url,
+        "filename": "%s.mp4/%s.mp4" % (youtube_id, youtube_id),
+        "public": 1,
+        "speed": 1,
+        "tuning": "animation",
+
+        # Video encoding options
+        "video_codec": "h264",
+        "quality": 5,
+
         "watermarks": [
             {
                 "width": 128,
@@ -63,16 +129,16 @@ def output_mp4(youtube_id, thumbnail_time):
         output["thumbnails"] = {
             "times": [thumbnail_time], 
             "public": 1,
-            "base_url": "{0}{1}.mp4/".format(BASE_URL, youtube_id),
+            "base_url": "{0}{1}.mp4/".format(base_url, youtube_id),
             "filename": youtube_id,
         }
 
     return output
 
-def output_m3u8_playlist(youtube_id, thumbnail_time):
+def output_m3u8_playlist(youtube_id, thumbnail_time, base_url):
     return {
 		"public": 1,
-		"base_url": BASE_URL,
+		"base_url": base_url,
         "filename": "%s.m3u8/%s.m3u8" % (youtube_id, youtube_id),
 		"streams": [
             {
@@ -91,31 +157,40 @@ def output_m3u8_playlist(youtube_id, thumbnail_time):
 		"type": "playlist"
 	}
 
-def output_m3u8_low(youtube_id, thumbnail_time):
+def output_m3u8_low(youtube_id, thumbnail_time, base_url):
     return {
+        "base_url": base_url,
+        "filename": "%s.m3u8/%s-low.m3u8" % (youtube_id, youtube_id),
         "public": 1,
+        "type": "segmented",
+
+        # Video encoding options
+        "skip_video": True,
+
+        # Audio encoding options
         "audio_channels": 1,
         "audio_quality": 3,
         "audio_normalize": True,
-        "skip_video": True,
-        "type": "segmented",
-        "base_url": BASE_URL,
-        "filename": "%s.m3u8/%s-low.m3u8" % (youtube_id, youtube_id),
     }
 
-def output_m3u8_medium(youtube_id, thumbnail_time):
+def output_m3u8_medium(youtube_id, thumbnail_time, base_url):
     return {
-		"video_codec": "h264",
 		"public": 1,
-        "audio_quality": 3,
-        "audio_normalize": True,
-        "quality": 4,
+        "base_url": base_url,
         "bitrate_cap": 120,
         "buffer_size": 480,
-		"max_video_bitrate": 100,
-		"type": "segmented",
-        "base_url": BASE_URL,
         "filename": "%s.m3u8/%s-medium.m3u8" % (youtube_id, youtube_id),
+        "type": "segmented",
+
+        # Video encoding options
+        "video_codec": "h264",
+        "quality": 4,
+        "max_video_bitrate": 100,
+
+        # Audio encoding options
+        "audio_quality": 3,
+        "audio_normalize": True,
+
         "watermarks": [
             {
                 "width": 128,
@@ -127,16 +202,64 @@ def output_m3u8_medium(youtube_id, thumbnail_time):
         ]
     }
 
-def output_m3u8_high(youtube_id, thumbnail_time):
+def output_m3u8_high(youtube_id, thumbnail_time, base_url):
     return {
-		"video_codec": "h264",
 		"public": 1,
+        "base_url": base_url,
+        "filename": "%s.m3u8/%s-high.m3u8" % (youtube_id, youtube_id),
+        "type": "segmented",
+
+        # Video encoding options
+        "video_codec": "h264",
+        "quality": 4,
+
+        # Audio encoding options
         "audio_quality": 3,
         "audio_normalize": True,
-        "quality": 4,
-		"type": "segmented",
-        "base_url": BASE_URL,
-        "filename": "%s.m3u8/%s-high.m3u8" % (youtube_id, youtube_id),
+
+        "watermarks": [
+            {
+                "width": 128,
+                "height": 16,
+                "x": -2,
+                "y": -2,
+                "url": "http://www.khanacademy.org/images/watermark.png",
+            }
+        ]
+    }
+
+def output_m3u8_playlist_aggressively_compressed(
+        youtube_id, thumbnail_time, base_url):
+    return {
+        "public": 1,
+        "base_url": base_url,
+        "filename": "%s.m3u8/%s-compressed.m3u8" % (youtube_id, youtube_id),
+        "streams": [{
+            "bandwidth": 64,
+            "path": "%s-compressed.m3u8" % youtube_id
+        }],
+        "type": "playlist"
+    }
+
+def output_m3u8_aggressively_compressed(youtube_id, thumbnail_time, base_url):
+    return {
+        "base_url": base_url,
+        "filename": "%s.m3u8/%s-compressed.m3u8" % (youtube_id, youtube_id),
+        "public": 1,
+        "type": "segmented",
+
+        # Video encoding options
+        "video_codec": "h264",
+        "crf": 40,
+        "max_frame_rate": 15,
+        "size": "640x480",
+
+        # Audio encoding options
+        "audio_codec": "aac",
+        "audio_bitrate": 8,  # Zencoder can't do lower than 8
+        "audio_channels": 1,
+        "audio_normalize": True,
+
         "watermarks": [
             {
                 "width": 128,
