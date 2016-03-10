12de798f407887de3d975dfe01eb570b67975274
Craig Silverstein <csilvers@khanacademy.org>
2016-03-10 11:16:48 -0800

    Fix a typo: it's util.logger to get our logger, not util.logging.
    Auditors: ben
    
    Test plan:
    Ran manually on youtube-export

--- print_missing_converted_formats.py
+++ print_missing_converted_formats.py
@@ -7,12 +7,12 @@ import util
 def main():
     util.setup_logging()
 
-    util.logging.info('Running s3.list_missing_converted_formats()')
+    util.logger.info('Running s3.list_missing_converted_formats()')
     yt = s3.list_missing_converted_formats()
 
     yt = sorted(y for y in yt if yt[y])
     if yt:
-        util.logging.error("MISSING CONVERTED FORMATS:\n%s" % "\n".join(yt))
+        util.logger.error("MISSING CONVERTED FORMATS:\n%s" % "\n".join(yt))
 
 
 if __name__ == '__main__':
