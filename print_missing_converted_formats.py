3bdd810745dee57ca0962422eb34ae1747b51c7f
Craig Silverstein <csilvers@khanacademy.org>
2016-04-19 11:22:02 -0700

    Improve print_missing_converted_formats to ignore new videos.
    Summary:
    If a video was *just* creatd, the converted formats often don't exist
    yet, since it takes time to do the conversion.  So now we only
    complain if a video is missing a conversion two days in a row.
    Hopefully it takes less than 24 hours to do a conversion!
    
    Test Plan:
    Fingers crossed -- we'll see what output this run has tomorrow.
    I will also check to make sure that the 'Saving converted-formats
    output' message is run every day.
    
    Reviewers: andy, chris
    
    Reviewed By: chris
    
    Subscribers: Jake
    
    Differential Revision: https://phabricator.khanacademy.org/D26687

--- print_missing_converted_formats.py
+++ print_missing_converted_formats.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import os
+import json
+import time
+
 import s3
 import util
 
@@ -8,11 +12,36 @@ def main():
     util.setup_logging()
 
     util.logger.info('Running s3.list_missing_converted_formats()')
-    yt = s3.list_missing_converted_formats()
+    today_yt = s3.list_missing_converted_formats()
 
-    yt = sorted(y for y in yt if yt[y])
+    # Because we will always be missing formats for new youtube videos
+    # that are still in the process of being converted, we only
+    # complain for videos that have ben missing a converted format for
+    # at least 2 days.
+    yesterday_fname = '/tmp/last_missing_converted.json'
+    if not os.path.exists(yesterday_fname):
+        util.logger.warn("Missing %s, will not report any missing converted "
+                         "formats today." % yesterday_fname)
+        yesterday_yt = {}
+    else:
+        with open(yesterday_fname) as f:
+            yesterday_yt = json.load(f)
+
+    # This limits the output to keys/values in both yesterday *and* today.
+    yt = {k: set(yesterday_yt[k]) & set(today_yt[k])
+          for k in set(yesterday_yt) & set(today_yt)}
+
+    yt = sorted("%s: %s" % (y, sorted(yt[y])) for y in yt if yt[y])
     if yt:
         util.logger.error("MISSING CONVERTED FORMATS:\n%s" % "\n".join(yt))
+
+    # Now write today's output out for tomorrow.  We only do this update
+    # once a day, where we round 'day' to 20 hours.
+    if (not os.path.exists(yesterday_fname) or
+            os.path.getmtime(yesterday_fname) + 20 * 60 * 60 < time.time()):
+        util.logger.info('Saving converted-formats output for use tomorrow')
+        with open(yesterday_fname, 'w') as f:
+            json.dump(today_yt, f)
 
 
 if __name__ == '__main__':
