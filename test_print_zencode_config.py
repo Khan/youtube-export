4d634f4d6a687f3faf869a7bf62c399b8ae1a318
Jason Pepas <jasonpepas@khanacademy.org>
2016-10-27 17:46:23 -0500

    Add a script to test-print the various Zencoder configs.
    Summary:
    This script can print out the various Zencoder configs.
    
    Example: `./test_print_zencode_config.py output_mp4_low`
    
    Test Plan:
    - When you run `./test_print_zencode_config.py output_mp4_low`, it should print the JSON config which would have been sent to Zencoder.com, which describes which settings to use when compressing the video, e.g:
    
    ```
    {
        "audio_bitrate": 4,
        "audio_codec": "amr",
        "audio_sample_rate": 8000,
        "base_url": "https://s3.amazonaws.com/SOME_S3_BUCKET/",
        "crf": 40,
        "filename": "FAKE_YTB_ID.mp4-low/FAKE_YTB_ID-low.mp4",
        "format": "3gp",
        "max_frame_rate": 15,
        "public": 1,
        "size": "640x480",
        "speed": 1,
        "thumbnails": {
            "base_url": "https://s3.amazonaws.com/SOME_S3_BUCKET/FAKE_YTB_ID.mp4-low/",
            "filename": "FAKE_YTB_ID",
            "public": 1,
            "times": [
                42
            ]
        },
        "tuning": "animation",
        "video_codec": "h264",
        "watermarks": [
            {
                "height": 16,
                "url": "http://www.khanacademy.org/images/watermark.png",
                "width": 128,
                "x": -2,
                "y": -2
            }
        ]
    }
    ```
    
    Reviewers: david, csilvers
    
    Reviewed By: david, csilvers
    
    Subscribers: csilvers
    
    Differential Revision: https://phabricator.khanacademy.org/D31691

---
+++ test_print_zencode_config.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+"""Print the JSON output of a given 'output_*' function in zencode.py.
+
+The 'output_*' functions in zencode.py generate JSON configurations which
+control Zencoder.com's behavior.  This script provides an easy mechanism to
+preview those JSON configurations (without actually submitting a Zencoder job).
+
+Example usage:
+    ./test_print_zencode_config.py output_mp4_low
+"""
+
+import argparse
+import json
+
+import zencode
+
+
+def pretty_print_dict(json_dict):
+    """Print a JSON dictionary using "pretty print" formatting."""
+    print json.dumps(json_dict, sort_keys=True, indent=4,
+                     separators=(',', ': '))
+
+
+def parse_arguments():
+    """Parse the command-line arguments.
+
+    One positional argument (the zencode.py 'output_*' function name) is
+    required.
+    """
+    parser = argparse.ArgumentParser(
+        description="Print JSON configurations sent to Zencoder.com")
+
+    parser.add_argument('function_name', help="The name of the zencode.py "
+                        "config function to test (e.g. 'output_mp4_low'")
+
+    return parser.parse_args()
+
+
+def print_zencode_config_with_fake_args(function):
+    """Evaluate and print a zencode.py 'output_*' config function.
+
+    Arguments:
+        function: a pointer to the 'output_*' function to call.
+    """
+    youtube_id = "FAKE_YTB_ID"
+    thumbnail_time = 42
+    base_url = "https://s3.amazonaws.com/FAKE_S3_BUCKET/"
+    zencoder_config = function(youtube_id, thumbnail_time, base_url)
+    pretty_print_dict(zencoder_config)
+
+
+def main():
+    args = parse_arguments()
+    function = getattr(zencode, args.function_name)
+    print_zencode_config_with_fake_args(function)
+
+
+if __name__ == "__main__":
+    main()
