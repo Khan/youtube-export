5e7094a9ed74a719cdf985bc6b1e49e70e5ce32b
Yeva Yu <yevayu@C02PF4XSG3QD.local>
2018-07-02 15:10:33 -0700

    Use proper path for running scripts
    Summary:
    We were originally using `$HOME/youtube-export` as the location of our files, but
    now that we clone youtube-export into internal-services before running our script,
    we need to update it accordingly!
    
    STOPSHIP: Actually, now that I think about it, it seems that our path is just plain wrong? Give me a second here.
    
    Test Plan: fingers crossed
    
    Reviewers: benkraft
    
    Reviewed By: benkraft
    
    Differential Revision: https://phabricator.khanacademy.org/D46134

--- cron/convert.sh
+++ cron/convert.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-cd $HOME/youtube-export
+# Figure out what directory we're *really* in (following symlinks).
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do   # follow symlinks
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"    # resolve relative symlink
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Go to export.py's directory
+cd $DIR/..
 
 python export.py --max=250
 
