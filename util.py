8c7cc03d775209c385557fc852276626d673b36f
Yeva Yu <yevayu@C02PF4XSG3QD.local>
2018-07-10 12:51:45 -0700

    Add stdout/stderr on logging so Stackdriver picks up the logs
    Summary:
    Since moving from AWS to GKE, we need to see our logs in Stackdriver! By default,
    all output to stdout is seen as INFO, and output to stderr is seen as ERROR in
    Stackdriver logs. If we find it really useful to have more than two levels of logs
    (eg: INFO/WARNING/ERROR), it might be worth using Google's python package for logging,
    but that seems like more work than it's worth for such a small service like this right
    now.
    
    Test Plan:
    Added the following code to the end of `util.py`
    
    ```
    setup_logging()
    logger.warning('THIS IS A WARNING')
    logger.info('hello again!')
    logger.critical("CRITICAL")
    print('umm')
    ```
    
    Ran `python util.py`
    Output:
    ```
    5ms THIS IS A WARNING
    5ms hello again!
    5ms CRITICAL
    umm
    ```
    
    Reviewers: csilvers, jacqueline
    
    Reviewed By: jacqueline
    
    Differential Revision: https://phabricator.khanacademy.org/D46293

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 import os
 import subprocess
-
+import sys
 
 DOWNLOADABLE_FORMATS = set(["mp4", "m3u8"])
 
@@ -25,6 +25,7 @@ logger = logging.getLogger("khan")
 def setup_logging(only_log_to_stdout=False, log_level=logging.DEBUG):
     assert not logger.handlers, "Should only call setup_logging once."
 
+    logger.setLevel(log_level)
     formatter = logging.Formatter(fmt='%(relativeCreated)dms %(message)s')
 
     def _add_handler(handler, level):
@@ -42,6 +43,20 @@ def setup_logging(only_log_to_stdout=False, log_level=logging.DEBUG):
         _add_handler(logging.FileHandler('logs/convert_%s.log' % strftime),
                      log_level)
 
-        # When logging to logs, also log serious errors to stderr
+        # When logging to logs, also log warnings and serious errors to stderr
         # (that way they're picked up by cron mail).
-        _add_handler(logging.StreamHandler(), logging.ERROR)
+        _add_handler(logging.StreamHandler(), logging.WARNING)
+
+        # Print to stdout on INFO level logs so that Stackdriver will log them.
+        # TODO(yeva): Use the google-cloud-logging package for logging.
+        info_logging_handler = logging.StreamHandler(stream=sys.stdout)
+        info_logging_handler.addFilter(_SingleLevelFilter(logging.INFO))
+        _add_handler(info_logging_handler, level=logging.INFO)
+
+
+class _SingleLevelFilter(logging.Filter):
+    def __init__(self, level):
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno == self.level
